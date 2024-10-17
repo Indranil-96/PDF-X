9d0086e8328a975b13863d608a9f18c69d42f2f3
Indranil Hazra <hazraindranil399@gmail.com>
2024-10-17 14:15:14 +0530

    project Setup

---
+++ server.js
@@ -0,0 +1,30 @@
+// Acquaring frameworks
+const express=require('express');
+const path= require('path');
+const ejs= require('ejs');
+const port=2000;
+
+
+
+const app = express();
+
+//Middleware
+app.set('view engien','ejs');
+app.use(express.static(path.join(__dirname,'view')));
+
+
+//Router
+
+app.get('/',(req,res)=>{
+    res.render('index');
+});
+
+
+// live Server
+app.listen(port,(err)=>{
+    if(err){
+        console.log('An error occured');
+    }else{
+        console.log(`App is listning to port ${port}`);
+    }
+})
